990da06ca44d1371f808f4e68be31ced4c36c435
Debriis <dhimanayush947@gmail.com>
2025-10-23 21:57:05 +0530

    Fk it

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import "./About.css";
-import nigger from '../../img/Ayush.jpg';
-import TextTrail from './TextTrail';
+import nigger from "../../img/Ayush.jpg";
+import TextTrail from "./TextTrail";
 
 const About = () => {
   return (
     <div className="a">
       <div className="a-left">
         <div className="a-card bg">
-         {<img src={nigger} alt="About me" className="a-img" /> }
-        </div>
-        <div className="a-card">
-          {<img src={cutieImg} alt="About me" className="a-img" /> }
+          <img src={nigger} alt="About me" className="a-img" />
         </div>
       </div>
+
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
-        <p className="a-sub">Heyoo guys </p>
+        <p className="a-sub">Heyoo guys</p>
 
-        <p className="a-desc"> I'm a MERN stack developer & B.Tech CSE student at SRM University, Chennai.</p>
-        <p className="a-desc"> I build scalable, responsive web apps using React, Node.js, MongoDB & Express.</p>
-        <p className="a-desc"> I love clean code, intuitive design, and seamless user experiences.</p>
-        <p className="a-desc"> I’m always exploring new tools, better UI/UX patterns, and smoother workflows.</p>
-        <p className="a-desc"> Open to internships, freelance projects, and collaborations with creative minds.</p>
-
-        
-       
+        <p className="a-desc">
+          I'm a MERN stack developer & B.Tech CSE student at SRM University,
+          Chennai.
+        </p>
+        <p className="a-desc">
+          I build scalable, responsive web apps using React, Node.js, MongoDB &
+          Express.
+        </p>
+        <p className="a-desc">
+          I love clean code, intuitive design, and seamless user experiences.
+        </p>
+        <p className="a-desc">
+          I’m always exploring new tools, better UI/UX patterns, and smoother
+          workflows.
+        </p>
+        <p className="a-desc">
+          Open to internships, freelance projects, and collaborations with
+          creative minds.
+        </p>
       </div>
     </div>
   );
